c3f8218632a473ef721ae79cbe23d2e0652bf647
ZSH4RK <158477225+ZSH4RK@users.noreply.github.com>
2025-05-09 12:37:08 +0100

    debugging practice problem 2

--- debugging practice.py
+++ debugging practice.py
@@ -1,3 +1,4 @@
+#debug 1
 print("Welcome to the Temperature converter!")
 print("This program converts between CElsius and Fahrenheit.")
 
@@ -22,3 +23,28 @@ while continue_program == "yes":
         print("invalid conversion choice")
 
     continue_program = input("do you want to continue yes/no")
+
+
+#debug 2
+print("welcome to the number guessing game")
+print("I'm thinking of a number from 1 to 100")
+
+secret_number = 42
+
+max_attempts = 5
+attempts = 0
+
+while attempts < max_attempts:
+    attempts += 1
+    print(f"Attempt {attempts} of {max_attempts}")
+    guess = int(input("Enter the guess "))
+
+    if guess == secret_number:
+        print("Correct")
+        break
+    elif guess < secret_number:
+        print("Too Low")
+
+    else:
+        print("too high")
+
